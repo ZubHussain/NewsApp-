03bcbd70e8a86176cda2a45b2f9567b773c13d81
Zubair <zuby4246@gmail.com>
2024-08-18 01:34:33 +0530

    loading function is added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from './Spinner'
 
 export class News extends Component {
   
@@ -13,65 +14,72 @@ export class News extends Component {
   }
   async componentDidMount(){
     let url="https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
-    console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
   }
     business = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/business/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
-    console.log(parseddata)
-    this.setState({articles: parseddata.articles})
-  
+    this.setState({articles: parseddata.articles,loading:false})
+    
+
   }
   entertainment = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/entertainment/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
    
   }
   general = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/general/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
    
   }
   health = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
    
   }
   science = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/science/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
     
   }
   sports = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
     
   }
   technology = async ()=>{
     let url="https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+    this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     console.log(parseddata)
-    this.setState({articles: parseddata.articles})
+    this.setState({articles: parseddata.articles,loading:false})
     
   }
   render() {
@@ -90,8 +98,9 @@ export class News extends Component {
           </div>
 
           <h2 className="row justify-content-center">NewsMonkey-Top Headlines</h2>
+          {this.state.loading && <Spinner/>}
         <div className="row">
-            {this.state.articles.map((element)=>{
+            {!this.state.loading && this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url} >
             <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} 
             imageUrl={element.urlToImage} newsUrl={element.url}/>
