70b7dfdb449f7e47c723a3816ff0211bc5d81dd3
Zubair <zuby4246@gmail.com>
2024-08-21 09:27:48 +0530

    Refactor the code in same function and heading showing cataegory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,9 +18,8 @@ export default class Navbar extends Component {
    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          
-          <Link className="nav-link" to="/general">General</Link>
+      <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/business">Business</Link>
@@ -30,6 +29,9 @@ export default class Navbar extends Component {
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/health">Health</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/general">General</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/entertainment">Entertainment</Link>
--- src/components/News.js
+++ src/components/News.js
@@ -3,53 +3,48 @@ import NewsItem from "./NewsItem";
 import Spinner from './Spinner'
 
 export class News extends Component {
+  capitalizeFirstLetter=(string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
   
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state={
       articles:[],
       loading:false,
       page:1
     }
+    document.title=this.capitalizeFirstLetter(this.props.category);
   }
-  async componentDidMount(){
-    
-    // let url=`https://saurav.tech/NewsAPI/top-headlines/category/${this.props.category}/${this.props.country}.json`
-    let url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&page=1&max=10&apikey=2cfe79fb6b164bf0a536b729da49bfc7&`
+  async updatePage(){
+    // const url=`https://saurav.tech/NewsAPI/top-headlines/category/${this.props.category}/${this.props.country}.json`
+    const url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&${this.props.page}&max=10&apikey=2cfe79fb6b164bf0a536b729da49bfc7&`
     this.setState({loading:true})
     let data=await fetch(url)
     let parseddata=await data.json();
     this.setState({articles: parseddata.articles,loading:false,totalresults:parseddata.totalresults})
   }
-  // handlePrev=async()=>{
-    
-  //   //  let url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&page=${this.state.page-1}&max=10&apikey=2cfe79fb6b164bf0a536b729da49bfc7&`
-  //   //  this.setState({loading:true})
-  //   //  let data=await fetch(url)
-  //   //  let parseddata=await data.json();
-  //   //  this.setState({articles: parseddata.articles,page:this.state.page-1,loading:false})
-   
-  // }
-  handleNext=async()=>{
-    // if(this.state.page+1 > Math.ceil(this.state.totalresults/10) ){
 
-    // }
-    // else{
-    //  let url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&page=${this.state.page+1}&max=10&apikey=2cfe79fb6b164bf0a536b729da49bfc7&`
-    //  this.setState({loading:true})
-    //  let data=await fetch(url)
-    //  let parseddata=await data.json();
-    //  this.setState({articles: parseddata.articles,page:this.state.page+1,loading:false})
-    // }
+  async componentDidMount(){
+    this.updatePage()
+  }
+  handlePrev=async()=>{
+    this.setState({page:this.state.page-1})
+    this.updatePage()
+   
+  }
+  handleNext=async()=>{
+    this.setState({page:this.state.page+1})
+    this.updatePage()
     alert("	This parameter will only work if you have a paid subscription activated on your account.")
     
   }
 
   render() {
     return (
-      
+
       <div className="container">
-          <h2 className="row justify-content-center">NewsMonkey-Top Headlines</h2>
+          <h2 className="row justify-content-center">NewsMonkey-{this.capitalizeFirstLetter(this.props.category)} Headlines</h2>
           {this.state.loading && <Spinner/>}
         <div className="row">
             {!this.state.loading && this.state.articles.map((element)=>{
@@ -62,7 +57,7 @@ export class News extends Component {
        })}
             <div className="container my-3 d-flex justify-content-between">
             <button type="button" disabled={this.state.page<=1} className="btn btn-dark btn-md" onClick={this.handlePrev}>&larr; Previous</button>
-            <button type="button" /*disabled={this.state.page+1 > Math.ceil(this.state.totalresults/10)}*/ className="btn btn-dark btn-md" onClick={this.handleNext}>Next &rarr;</button>
+            <button type="button" disabled={this.state.page+1 > Math.ceil(this.state.totalresults/10)} className="btn btn-dark btn-md" onClick={this.handleNext}>Next &rarr;</button>
             </div>
          
         </div>
